7e243a9e14406b3dcae2732182aea1476dbf4b03
zhaoxk <xkzalpha@gmail.com>
2013-12-08 22:40:35 +0800

    make the code clean

--- static/js/myjs.js
+++ static/js/myjs.js
@@ -1,207 +1,208 @@
+/* Control the watch page */
+
+/* Actions when document is ready */
 $(document).ready(function() {
-    pauseCarousel();
-    firstItemActive();
-    $('#addtips').on('click', addTips);
-    $('#edittips').on('click', editTips);
-    $('#editaction').on('click', editPrepare);
-    $('#deletetips').on('click', deleteTips);
-    $('.carousel-control.right').on('click', nextCarousel);
-    $('.carousel-control.left').on('click', preCarousel);
+  pauseCarousel();
+  firstItemActive();
+  $('#addtips').on('click', addTips);
+  $('#edittips').on('click', editTips);
+  $('#editaction').on('click', editPrepare);
+  $('#deletetips').on('click', deleteTips);
+  $('.carousel-control.right').on('click', nextCarousel);
+  $('.carousel-control.left').on('click', preCarousel);
 });
 
-/* Carousel Control Action */
+/* Update progress bar */
+function updateProgressBar(getNewProgress) {
+  var pg = $('.progress');
+  var pb = $('.progress-bar');
+  var inner = $('.carousel-inner');
+
+  var widthMax = pg.width();
+  var width = pb.width();
+  var chidNum = inner.children().length;
+
+  var step = widthMax/chidNum;
+  var newProg = getNewProgress(width, widthMax, step, chidNum);
+  pb.css('width', newProg);
+}
+
+/* Switch to previous carousel item */
 function preCarousel() {
-    var pg = $('.progress');
-    var pb = $('.progress-bar');
-    var inner = $('.carousel-inner');
-
-    var width_max = pg.width();
-    var width = pb.width();
-    var chidNum = inner.children().length;
-
-    var step = width_max/chidNum;
-    var newProg = Math.max (width - step, 0);
-    pb.css('width', newProg);
+  function getNewProgress(width, widthMax, step, chidNum) {
+    return Math.max(width - step, 0);
+  }
+  updateProgressBar(getNewProgress);
 }
 
+/* Switch to next carousel item */
 function nextCarousel() {
-    var pg = $('.progress');
-    var pb = $('.progress-bar');
-    var inner = $('.carousel-inner');
-
-    var width_max = pg.width();
-    var width = pb.width();
-    var chidNum = inner.children().length;
-
-    var step = width_max/chidNum;
-    var newProg = Math.min (width + step, width_max);
-    pb.css('width', newProg);
+  function getNewProgress(width, widthMax, step, chidNum) {
+    return Math.min (width + step, widthMax);
+  }
+  updateProgressBar(getNewProgress);
 }
 
+/* Update progress bar after add tips */
 function updateProgressAfterAdd() {
-    var pg = $('.progress');
-    var pb = $('.progress-bar');
-    var inner = $('.carousel-inner');
-
-    var width_max = pg.width();
-    var width = pb.width();
-    var chidNum = inner.children().length;
-
-    var step = width_max / chidNum;
-    var preStep = width_max / (chidNum-1);
+  function getNewProgress(width, widthMax, step, chidNum) {
+    var preStep = widthMax / (chidNum-1);
     var progNum = width / preStep;
-    var newProg = Math.max (step*progNum, 0);
-    pb.css('width', newProg);
+    return Math.max (step*progNum, 0);
+  }
+  updateProgressBar(getNewProgress);
 }
 
+/* Update progress bar after delete tips */
 function updateProgressAfterDelete() {
-    var pg = $('.progress');
-    var pb = $('.progress-bar');
-    var inner = $('.carousel-inner');
-
-    var width_max = pg.width();
-    var width = pb.width();
-    var chidNum = inner.children().length;
-
-    var step = width_max/chidNum;
-    var preStep = width_max / (chidNum+1);
+  function getNewProgress(width, widthMax, step, chidNum) {
+    var preStep = widthMax / (chidNum+1);
     var progNum = width / preStep;
-    var newProg = Math.min (step*progNum, width_max);
-    pb.css('width', newProg);
+    return Math.min (step*progNum, widthMax);
+  }
+  updateProgressBar(getNewProgress);
 }
 
 /* Set tips content position: center */
 function setTipsPosition() {
-    var wrapper = $('.tips-content-wrapper');
-    var height = wrapper.height();
-    wrapper.css('marginTop', -1 * height / 2);
-    var width = wrapper.width();
-    wrapper.css('marginLeft', -1 * width / 2);
+  var wrapper = $('.tips-content-wrapper');
+  var height = wrapper.height();
+  wrapper.css('marginTop', -1 * height / 2);
+  var width = wrapper.width();
+  wrapper.css('marginLeft', -1 * width / 2);
 }
 
 /* Disable carousel auto play */
 function pauseCarousel() {
-    $('.carousel').carousel('pause');
+  $('.carousel').carousel('pause');
 }
 
 /* Active first child of the carousel which is created dynamic */
 function firstItemActive() {
-    $('.carousel-inner .item:first-child')
-        .addClass("active");
+  $('.carousel-inner .item:first-child')
+    .addClass("active");
+}
+
+/* Append items to carousel-inner */
+function appendItemToCarousel(inner, id, title, content) {
+    inner.append(
+      '<div class="item" id="tips-' + id + '">' +
+        '<img data-src="holder.js/900x500/auto/#777:#777/">' +
+          '<div class="carousel-caption tips-content">' +
+          '<div class="tips-content-wrapper">' +
+            '<h1>' + title + '</h1>' +
+            '<p>' + content + '</p>' +
+          '</div>' +
+          '</div>' +
+        '</div>'
+    );
 }
 
 /* Add new tips for the current category */
 function addTips() {
-    $('#addModal').modal('hide');
-    var title = $('#tips-title').val();
-    var content = $('#new-tips-content').val();
-    var category = $('#category-value').val();
-    var data = {
-        'category': category,
-        'title': title,
-        'content': content
-    };
-    $.post('/add', data, function(gdata) {
-        console.log(gdata);
-        var inner = $('.carousel-inner')
-        if (gdata['action'] == "replace") {
-            inner.empty()
-        }
+  $('#addModal').modal('hide');
+  var title = $('#tips-title').val();
+  var content = $('#new-tips-content').val();
+  var category = $('#category-value').val();
+  var data = {
+    'category': category,
+    'title': title,
+    'content': content
+  };
+  $.post('/add', data, function(gdata) {
+    console.log(gdata);
+    var inner = $('.carousel-inner')
+    if (gdata['action'] == "replace") {
+      inner.empty()
+    }
 
-        inner.append(
-            '<div class="item" id="tips-' + gdata['id'] + '">\
-                <img data-src="holder.js/900x500/auto/#777:#777/">\
-                  <div class="carousel-caption tips-content">\
-                    <div class="tips-content-wrapper">\
-                      <h1>' + title + '</h1>\
-                      <p>' + content + '</p>\
-                    </div>\
-                  </div>\
-              </div>'
-        );
+    appendItemToCarousel(inner, gdata['id'], title, content);
+    if (inner.children().length == 1) {
+      firstItemActive();
+    }
 
-        Holder.run();
-        $('.carousel').carousel("pause").removeData();
-        pauseCarousel();
-        updateProgressAfterAdd();
-    }, 'json');
+    Holder.run();
+    $('.carousel').carousel("pause").removeData();
+    pauseCarousel();
+    updateProgressAfterAdd();
+  }, 'json');
 }
 
 /* Prepare for the edit, fill the textarea of the Edit Modal */
 function editPrepare() {
-    var activeTips = $(".carousel-inner .item.active .tips-content-wrapper").children();
-    var title = activeTips[0].innerHTML;
-    var content = activeTips[1].innerHTML;
-    $("#editModal #tips-title").val(title);
-    $("#editModal #new-tips-content").val(content);
+  var activeTips = $(".carousel-inner .item.active .tips-content-wrapper")
+    .children();
+  var title = activeTips[0].innerHTML;
+  var content = activeTips[1].innerHTML;
+  $("#editModal #tips-title").val(title);
+  $("#editModal #new-tips-content").val(content);
 }
 
 
 /* Edit tips for the current category */
 function editTips() {
-    $('#editModal').modal('hide');
-    var tipsID = $(".carousel-inner .item.active").attr("id");
-    var id = tipsID.split('-')[1];
-    var title = $('#editModal #tips-title').val();
-    var content = $('#editModal #new-tips-content').val();
-    var category = $('#category-value').val();
-    var data = {
-        'category': category,
-        'id': id,
-        'title': title,
-        'content': content
-    };
-    $.post('/edit', data, function(gdata) {
-        if (gdata['action'] == 'update') {
-            var activeTips = $(".carousel-inner .item.active .tips-content-wrapper").children();
-            activeTips[0].innerHTML = title;
-            activeTips[1].innerHTML = content;
-        }
-    }, 'json'); 
+  $('#editModal').modal('hide');
+  var tipsID = $(".carousel-inner .item.active").attr("id");
+  var id = tipsID.split('-')[1];
+  var title = $('#editModal #tips-title').val();
+  var content = $('#editModal #new-tips-content').val();
+  var category = $('#category-value').val();
+  var data = {
+    'category': category,
+    'id': id,
+    'title': title,
+    'content': content
+  };
+  $.post('/edit', data, function(gdata) {
+    if (gdata['action'] == 'update') {
+      var activeTips = $(".carousel-inner .item.active .tips-content-wrapper")
+        .children();
+      activeTips[0].innerHTML = title;
+      activeTips[1].innerHTML = content;
+    }
+  }, 'json'); 
 }
 
 /* Delete tips */
 function deleteTips() {
-    $('#deleteModal').modal('hide');
-    var tipsID = $(".carousel-inner .item.active").attr("id");
-    var id = tipsID.split('-')[1];
-    var category = $('#category-value').val();
-    var data = {
-        'category': category,
-        'id': id,
-    };
-    $.post('/delete', data, function(gdata) {
-        if (gdata['action'] == 'delete') {
-            var inner = $('.carousel-inner');
-            var default_tips = JSON.parse(gdata['default']);
-            var dt_len = default_tips.length;
-            var curItem = $(".carousel-inner .item.active");
-            var nextItem = curItem.next();
-            curItem.remove();
-            if (inner.children().length == 0) {
-                for (var i=0; i<dt_len; i++) {
-                    inner.append(
-                        '<div class="item" id="tips-' + default_tips[i]['id'] + '">\
-                            <img data-src="holder.js/900x500/auto/#777:#777/">\
-                              <div class="carousel-caption tips-content">\
-                                <div class="tips-content-wrapper">\
-                                  <h1>' + default_tips[i]['title'] + '</h1>\
-                                  <p>' + default_tips[i]['content'] + '</p>\
-                                </div>\
-                              </div>\
-                          </div>'
-                    );
-                }
-                firstItemActive();
-                Holder.run();
-                $('.carousel').carousel("pause").removeData();
-                pauseCarousel();
-            } else {
-                nextItem.addClass("active");
-                updateProgressAfterDelete();
-            }
+  $('#deleteModal').modal('hide');
+  var tipsID = $(".carousel-inner .item.active").attr("id");
+  var id = tipsID.split('-')[1];
+  var category = $('#category-value').val();
+  var data = {
+    'category': category,
+    'id': id,
+  };
+  $.post('/delete', data, function(gdata) {
+    if (gdata['action'] == 'delete') {
+      var inner = $('.carousel-inner');
+      var default_tips = JSON.parse(gdata['default']);
+      var dt_len = default_tips.length;
+      var curItem = $(".carousel-inner .item.active");
+      var nextItem = curItem.next();
+      curItem.remove();
+      if (inner.children().length == 0) {
+        for (var i=0; i<dt_len; i++) {
+          appendItemToCarousel(
+            inner, 
+            default_tips[i]['id'], 
+            default_tips[i]['title'], 
+            default_tips[i]['content']
+            );
         }
-    }, 'json'); 
+
+        firstItemActive();
+        Holder.run();
+        $('.carousel').carousel("pause").removeData();
+        pauseCarousel();
+      } else {
+        if (nextItem.length) {
+          nextItem.addClass("active");
+        } else {
+          firstItemActive();
+        }
+        updateProgressAfterDelete();
+      }
+    }
+  }, 'json'); 
 }
-
-
