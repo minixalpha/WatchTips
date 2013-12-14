8564e972d747c66eb957dc25d082655a65136409
zhaoxk <xkzalpha@gmail.com>
2013-12-14 23:30:31 +0800

    fix commit error

--- config_sample.py
+++ config_sample.py
@@ -3,12 +3,8 @@
 import web
 
 render = web.template.render('app/views/', base='layout')
-<<<<<<< HEAD
 
 DATABASE_TYPE = 'mysql'
 DATABASE_NAME = 'watchtips'
 DATABASE_USERNAME = 'proj'
 DATABASE_PASSWORD = 'XXX'
-=======
-db = web.database(dbn='mysql', db='watchtips', user='proj', pw='XXX')
->>>>>>> 791d8beb9348769d137a51dc657431c01113a9fd
