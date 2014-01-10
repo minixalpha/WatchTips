3c906bb4dcc16be0ab1c8a6c6ce7db86fa2b1e28
zhaoxk <xkzalpha@gmail.com>
2014-01-10 20:22:00 +0800

    update to new version configuration

--- config_sample.py
+++ config_sample.py
@@ -1,8 +1,20 @@
 #coding:utf-8
 
+
+DEBUG = True
 DATABASE_TYPE = 'mysql'
 DATABASE_NAME = 'watchtips'
 DATABASE_USERNAME = 'proj'
-DATABASE_PASSWORD = 'XXXX'
+DATABASE_PASSWORD = 'XXX'
 
-DEBUG = True
+log_enable = True
+log_file = 'log/wtlog.log'
+log_tofile = True
+log_toprint = True
+
+ssl_enable = True
+ssl_certificate_path = 'crtkey/server.crt'
+ssl_private_key_path = 'crtkey/server.key'
+
+sessionid_name = 'user_session'
+session_dir = 'sessions'
